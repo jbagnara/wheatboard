12e8f21ed3ac96f8e949fdc1b8884d9d81b55874
clippy <jason@papaya.red>
2025-08-22 22:02:44 -0400

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [1] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MINS, KC_EQL,     KC_BSPC,                KC_PGUP,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MINS, KC_EQL,     KC_DELETE,              KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_PSCR, KC_LBRC, KC_RBRC,    KC_BSLS,                KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,                 KC_END,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    MACRO_0, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,    KC_UP,      KC_HOME,
